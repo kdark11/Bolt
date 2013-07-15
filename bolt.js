0414b41728982d0ed192de83f50d2a95067df5b7
kdark11 <k.dark11@gmail.com>
2013-07-15 05:42:45 +0000

    logging request

--- bolt.js
+++ bolt.js
@@ -1,6 +1,7 @@
 
 var http = require("http");
 function onRequest(request, response){
+	console.log("Request Received.");
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("Hello World");
 	response.end();
