13821c97ee50845140aeca1f1b7619cb17c28b37
kdark11 <k.dark11@gmail.com>
2013-07-15 06:26:47 +0000

    added an index.js file and router.js to the app

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 
 var http = require("http");
+var url = require("url");
 
-function start(){
+function start(){  //created a module to start the server
 	function onRequest(request, response){
-		console.log("Request Received.");
+		var pathname = url.parse(request.url).pathname; //distinguishes request based on url path requested
+		console.log("Request for " + pathname + " Received.");
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("Hello World");
 		response.end();
 	}
 
-	http.createServer(onRequest).listen(process.env.PORT || 5000);
+	http.createServer(onRequest).listen(process.env.PORT || 5000); //createServer returns(starts) a new web server object
+	//onRequest is the call back triggered folling the createServer command
 	console.log("Server has started.");
 }
 exports.start = start;
