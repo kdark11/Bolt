08f4009c9f4a82080526bf419251b0d1b4e4272b
kdark11 <k.dark11@gmail.com>
2013-07-24 07:32:51 +0000

    Completed Nodebeginner but on an EC2 instance

--- server.js
+++ server.js
@@ -5,16 +5,11 @@ var url = require("url");
 function start(route, handle){  //created a module to start the server
 	function onRequest(request, response){
 		var pathname = url.parse(request.url).pathname; //distinguishes request based on url path requested
-		console.log("Request for " + pathname + " Received.");
-
-		route(handle, pathname); //route callback
-
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Hello World");
-		response.end();
+		console.log("Request for " + pathname + " received.");
+		route(handle, pathname, response, request);
 	}
 
-	http.createServer(onRequest).listen(process.env.PORT || 5000); //createServer returns(starts) a new web server object
+	http.createServer(onRequest).listen(process.env.PORT || 8080); //createServer returns(starts) a new web server object
 	//onRequest is the call back triggered folling the createServer command
 	console.log("Server has started.");
 }
